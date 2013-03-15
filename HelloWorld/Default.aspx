87ff8a82a37c3cc8265996a1fb5fc11756a227f1
Eric Liu <eric@1stlight.us>
2013-03-15 17:16:01 +0800

    my first commit

---
+++ HelloWorld/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="HelloWorld._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        Welcome to ASP.NET! 
+    </h2>
+    <p>
+       This is Eric's testing for auto-building! 
+    </p>
+    <p>
+        1st Light Tech.
+    </p>
+    A: <asp:TextBox ID="TextBox1" runat="server" AutoPostBack="True" 
+    ontextchanged="TextBox1_TextChanged"></asp:TextBox>
+    B: <asp:TextBox ID="TextBox2" runat="server" AutoPostBack="True"
+    ontextchanged="TextBox2_TextChanged"></asp:TextBox>
+    <asp:Button ID="Button1" runat="server" Text="Submit" onclick="Button1_Click"/>
+  
+    <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+    <asp:Label ID="Label2" runat="server" Visible="False"></asp:Label>
+    
+    <asp:HiddenField ID="HiddenField1" runat="server" Value="false" />
+    <asp:HiddenField ID="HiddenField2" runat="server" Value="false" />
+
+</asp:Content>
---
+++ HelloWorld/Default.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloWorld
+{
+
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_PreLoad(object sender, EventArgs e)
+        {/*
+            for (int i = 0; i < 15; i++)
+                Response.Write("a" + i + " ");
+            Response.Write("<br>");
+          */
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {/*
+            if (Response.IsClientConnected)
+            {
+                Response.Write("This is Page_Load()<br>");
+            }
+          */
+            //Response.Redirect("http://tw.yahoo.com");
+            Button1.Enabled = false;
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {   
+            int intTemp = 0;
+            if (string.IsNullOrEmpty(TextBox1.Text))
+            {
+                Label1.Text = "輸入值為空!";
+            }
+            else if (!int.TryParse(TextBox1.Text , out intTemp))
+            {
+                Label1.Text = "數值A不為數字!";
+            }
+            else
+            {
+                Label1.Text = "";
+                HiddenField1.Value = "*";
+                Label2.Text = HiddenField1.Value + HiddenField2.Value;
+                Button1.Enabled = (Label2.Text == "**") ? true : false;
+            }
+        }
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            int intTemp = 0;
+            if (string.IsNullOrEmpty(TextBox2.Text))
+            {
+                Label1.Text = "輸入值為空!";
+            }
+            else if (!int.TryParse(TextBox2.Text, out intTemp))
+            {
+                Label1.Text = "數值B不為數字!";
+            }
+            else
+            {
+                Label1.Text = "";
+                HiddenField2.Value = "*";
+                Label2.Text = HiddenField1.Value + HiddenField2.Value;
+                Button1.Enabled = (Label2.Text == "**") ? true : false;
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int sum = Convert.ToInt32(TextBox1.Text) + Convert.ToInt32(TextBox2.Text);
+            Response.Write("<script>alert(" + sum + ");</script>");
+        }
+    }
+}
